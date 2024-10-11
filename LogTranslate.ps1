918cca2c7cbb11f7a1b349ee048e37d2dfa50a69
Dani <64833443+conecta2enred@users.noreply.github.com>
2024-10-11 10:15:53 +0200

    Add files via upload

---
+++ LogTranslate.ps1
@@ -0,0 +1,88 @@
+﻿# SCRIPT CONECTA2 EN RED #
+# Traductor se encarga de tomar los datos del log de Control de RED generados por PortControl.ps1 para traducir las IP por su resolución de DNS y los procesos
+# por el nombre de proceso al que pertenece.
+# Si no reconoce la DNS lo indicará en el Script. En el caso de no resolver el nombre del proceso, quiere decir que en el momento de la ejecución del
+# traductor este no estaba funcionando. Esta puede ser una alerta y es conveniente comprobar con más detalle este caso.
+
+# Ruta del archivo de registro original. Modificar por lugar donde añadiste el log
+$originalLogPath = "C:\Users\user\Desktop\log.log"
+
+# Ruta del archivo de traducción. Modificar por lugar donde guardar el log de traducción
+$traduccionLogPath = "C:\Users\user\Desktop\traduccion.log"
+
+# Leer el contenido del archivo de registro original
+$logContent = Get-Content -Path $originalLogPath
+
+# Función para traducir la dirección IP a nombre de host
+function Translate-IPAddress {
+    param (
+        [string]$IPAddress
+    )
+
+    try {
+        $hostName = (Resolve-DnsName -Name $IPAddress).NameHost
+        if ($hostName) {
+            return $hostName
+        } else {
+            return "No Detectado DNS"
+        }
+    } catch {
+        return "No Detectado DNS"
+    }
+}
+
+# Función para obtener el nombre del proceso a partir del ID de proceso (PID)
+function Get-ProcessName {
+    param (
+        [int]$PIDF
+    )
+
+    $process = Get-Process -Id $PIDF -ErrorAction SilentlyContinue
+    if ($process) {
+        return $process.ProcessName
+    } else {
+        return "Alerta Proceso No reconocido"
+    }
+}
+
+# Inicializar el archivo de traducción. Si no queremos este valor, podemos comentar la siguiente linea.
+$logContent[0..1] | Out-File -FilePath $traduccionLogPath
+
+# Traducir y registrar las conexiones
+for ($i = 4; $i -lt $logContent.Count; $i++) {
+    if ($logContent[$i] -match '^  (TCP|UDP)\s+(\S+:\d+)\s+(\S+:\d+)\s+(\S+)\s+(\d+)$') {
+        $protocol = $matches[1]
+        $localAddress = $matches[2]
+        $remoteAddress = $matches[3]
+        $state = $matches[4]
+        $pidn = $matches[5]
+
+        $localAddressComponents = $localAddress -split ':'
+        $remoteAddressComponents = $remoteAddress -split ':'
+
+        $translatedLocalAddress = "$($localAddressComponents[0]):$($localAddressComponents[1])"
+        $translatedRemoteAddress = "$($remoteAddressComponents[0]):$($remoteAddressComponents[1])"
+
+        $translatedRemoteHostName = Translate-IPAddress -IPAddress $remoteAddressComponents[0]
+        $processName = Get-ProcessName -PID $pidn
+
+        #Podemos modificar el logentry para que muestre los valores que nos interesa.
+
+        $logEntry = "$protocol ; $translatedLocalAddress ; $translatedRemoteAddress ; $state ; $pidn ; $translatedRemoteHostName ; $processName"
+        $logEntry | Out-File -Append -FilePath $traduccionLogPath
+    }elseif($logContent[$i] -match '^\s*(UDP)\s+((\[([^\]]+)\]|(\d+\.\d+\.\d+\.\d+)):(\d+))\s+(\S+)\s+(\d+)$'){
+        #podemos añadir más valores de manera similar al control de TCP anterior.
+        $protocol = $matches[1]
+        $pidn = $matches[8]
+        $processName = Get-ProcessName -PID $pidn
+        #Podemos definir el logentry con los valores que queramos mostrar
+        $logEntry = "$protocol ; $pidn ; $processName"
+        $logEntry | Out-File -Append -FilePath $traduccionLogPath
+    
+    }else{
+        $logContent[$i] | Out-File -Append -FilePath $traduccionLogPath
+    }
+}
+
+
+Write-Output "Traducción completada."
---
+++ PortControl.ps1
@@ -0,0 +1,81 @@
+﻿# SCRIPT CONECTA2 EN RED #
+# Control de Red analiza mediante Netstat durante un periodo de tiempo las conexiones del equipo para encontrar una conexion no identificada que pueda
+# pertenecer a Bot-net/malware en el sistema.
+
+# Path donde guardar el log. Utiliza tu ruta.
+$filePath = "C:\Users\user\Desktop\log.log"
+
+# Netstat
+$initialConnections = netstat -ano
+
+# Inicializar la lista de conexiones anteriores
+$lastConnections = $initialConnections
+
+# Log Register Primer Netstat
+$initialConnections | Out-File -Append -FilePath $filePath
+
+# Bucle for para ejecutar el comando durante 20 segundos. Puedes cambiar el valor 20 de -le
+# para poner más o menos tiempo.
+for ($i = 1; $i -le 20; $i++) {
+    # Obtener las conexiones actuales
+    $currentConnections = netstat -ano
+
+    # Determinar conexiones añadidas y quitadas
+    $addedConnections = Compare-Object -ReferenceObject $lastConnections -DifferenceObject $currentConnections -PassThru
+    $removedConnections = Compare-Object -ReferenceObject $currentConnections -DifferenceObject $lastConnections -PassThru
+
+    # Registrar tiempo de consulta
+    Write-Output "Segundo $i" | Out-File -Append -FilePath $filePath
+
+    # Registrar conexiones añadidas
+    if ($addedConnections.Count -gt 0) {
+        Write-Output "Direcciones añadidas:" | Out-File -Append -FilePath $filePath
+        $addedConnections | Out-File -Append -FilePath $filePath
+    }
+
+    # Registrar conexiones quitadas
+    if ($removedConnections.Count -gt 0) {
+        Write-Output "Direcciones quitadas:" | Out-File -Append -FilePath $filePath
+        $removedConnections | Out-File -Append -FilePath $filePath
+    }
+
+    # Actualizar la lista de conexiones anteriores
+    $lastConnections = $currentConnections
+
+    # Esperar 1 segundo antes de la próxima iteración
+    Start-Sleep -Seconds 1
+}
+
+# Bucle for para analizar cada minuto hasta 10 minutos. Puedes cambiar el valor
+# -le para añadir más tiempo
+for ($j = 1; $j -le 10; $j++) {
+    # Esperar 1 minuto antes de la próxima iteración
+    Start-Sleep -Seconds 60
+
+    # Obtener las conexiones actuales
+    $currentConnections = netstat -ano
+
+    # Registrar tiempo de consulta
+    Write-Output "Minuto $j" | Out-File -Append -FilePath $filePath
+
+    # Determinar conexiones añadidas y quitadas
+    $addedConnections = Compare-Object -ReferenceObject $lastConnections -DifferenceObject $currentConnections -PassThru
+    $removedConnections = Compare-Object -ReferenceObject $currentConnections -DifferenceObject $lastConnections -PassThru
+
+    # Registrar conexiones añadidas
+    if ($addedConnections.Count -gt 0) {
+        Write-Output "Direcciones añadidas:" | Out-File -Append -FilePath $filePath
+        $addedConnections | Out-File -Append -FilePath $filePath
+    }
+
+    # Registrar conexiones quitadas
+    if ($removedConnections.Count -gt 0) {
+        Write-Output "Direcciones quitadas:" | Out-File -Append -FilePath $filePath
+        $removedConnections | Out-File -Append -FilePath $filePath
+    }
+
+    # Actualizar la lista de conexiones anteriores
+    $lastConnections = $currentConnections
+}
+
+Write-Output "Registro completado."
